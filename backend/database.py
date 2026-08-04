44504e0e9adc6b576e6e3e7595ea6bdf4b15e3b0
Kulcsar Delia <kulcsardelia77@gmail.com>
2026-08-04 20:30:25 +0300

    Structurat database.py, models.py si configurat rutele in main.py

---
+++ backend/database.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+load_dotenv()
+
+DB_SERVER = os.getenv("DB_SERVER")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_DRIVER = os.getenv("DB_DRIVER", "ODBC Driver 18 for SQL Server")
+
+SQLALCHEMY_DATABASE_URL = (
+    f"mssql+pyodbc://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}/{DB_NAME}?"
+    f"driver={DB_DRIVER}&Encrypt=yes&TrustServerCertificate=no&Connection+Timeout=30"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends
 from parser import parse_file
 from fastapi.middleware.cors import CORSMiddleware
+from database import get_db, engine
+
 
 app = FastAPI()
 
@@ -20,3 +21,4 @@ app.add_middleware(
 @app.get("/api/status")
 def read_status():
     return {"status": "Conectat cu succes la backend!"}
+
